4653e8a6f070b5e35580503cd19c0bee0c681e4b
AdamFASHOO <AdamChapman1994@gmail.com>
2022-05-12 16:17:00 -0400

    Coffee Shop Lab part 1 Finish

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(Registrant r)
+        {
+            return View(r);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
